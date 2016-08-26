33ad209b4550cd5c001b3dba2fbeca5a16185d4c
Patrick Kennedy <mixelpix@mac.com>
2016-08-26 01:56:16 -0400

    moving along

--- LRIOV_Banas.rb
+++ LRIOV_Banas.rb
@@ -8,14 +8,34 @@ Everything is an object and all of those objects can be overwritten(?what does t
 =end
 
 # print prints the string to screen WITHOUT a newline
+p "The 'p' command prints STRINGS with quotes and a newline"
+puts "The 'puts' command prints with a newline and no quotes."
 print "Enter a Number: "
-
-# Variables start with a lowercase letter (by convention) or _ and may contain numbers
-# "gets" stores input from the user and to_i turns the input into an integer
-first_num = gets.to_i
 
 
 # Get Input 1:47
+# Variables start with a lowercase letter (by convention) or _ and may contain numbers
+# "gets" stores input from the user and to_i turns the input into an integer
+first_num = gets.to_i
+p first_num # NOTE: NO QUOTES WITH INTEGERS
+puts "You enetered: %s" % first_num
+puts "With gets.to_i is this a string? %s" % first_num.is_a?(String)
+puts "...or this now an integer? %s" % first_num.is_a?(Fixnum)
+
+print "Enter Another Number: "
+another_num = gets
+p another_num # NOTE: QUOTES
+puts "Get it? 'gets' without '.to_i' reads the number as a string!"
+p "And... this string comes in with a 'newline': %s" % another_num # NOTE: I use 'p' so the newline is displayed
+puts "Is this a string? %s" % another_num.is_a?(String)
+puts "Is this an integer? %s" % another_num.is_a?(Fixnum)
+
+print "Enter Another Number: "
+second_num = gets.to_i
+
+# puts prints output plus a newline; to_s converts a variable into a string
+puts first_num.to_s + " + " + second_num.to_s + " = " +
+(first_num + second_num).to_s
 
 
 # Arithmetic 3:11
